34202c852ab6279fbba275f629a4bc4ff7fe3e01
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-08 20:32:26 +0200

    Created interface for the data cleaner service.

---
+++ lolpetprojectCSAPI/Interfaces/IDataSortHelperService.cs
@@ -0,0 +1,9 @@
+﻿using lolpetprojectCSAPI.Models.MatchDataSpecific;
+
+namespace lolpetprojectCSAPI.Interfaces
+{
+    public interface IDataSortHelperService
+    {
+        SortedMatchData SortMatchData(MatchSpecific matchSpecific, long championId);
+    }
+}
