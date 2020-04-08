fc38c0ed1cd298794200886547d92b1bb847bbd0
Gergely Álmos Koltai <kgalmos@gmail.com>
2020-04-08 20:33:58 +0200

    Created model for the new sorted data.

---
+++ lolpetprojectCSAPI/Models/MatchDataSpecific/SortedMatchData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace lolpetprojectCSAPI.Models.MatchDataSpecific
+{
+    public class SortedMatchData
+    {
+
+        public double Kda { get; set; }
+        public int KillParticipation { get; set; }
+        public long Kills { get; set; }
+        public long Assists { get; set; }
+        public long Deaths { get; set; }
+        public TimeSpan GameLength { get; set; }
+        public bool Win { get; set; }
+        public long MinionsKilled { get; set; }
+        public double CsPerMin { get; set; }
+        public double VisionScorePerMin { get; set; }
+        public double DamagePerMin { get; set; }
+        public decimal DamagePercentOfTotal { get; set; }
+        public long QueueId { get; set; }
+        
+        public string Role { get; set; }
+    }
+}
